70e2855995ebb80c130d19af87b2c5755f9e10f7
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-03-15 01:13:21 -0300

    Utilizando PSR-4

--- psr-4/public/index.php
+++ psr-4/public/index.php
@@ -2,10 +2,11 @@
 
 require_once '../vendor/autoload.php';
 
-use  App\Controllers\HomeController as Home;
+use App\Controllers\HomeController as Home;
+use ETI\DB\Database;
 
 $home = new Home;
 //echo $home->index();
 
-$database = new \ETI\DB\Database;
+$database = new Database;
 var_dump($database->getConn());
